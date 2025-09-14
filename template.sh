6b59b12ba71f697d5bf89d814697d11074610ffc
yash <yashkhandelwal771@gmail.com>
2025-09-14 12:47:17 +0530

    folder structure created

---
+++ template.sh
@@ -0,0 +1,17 @@
+# creating directory for the projects
+
+mkdir -p src
+mkdir -p research
+
+# creating files
+
+touch src/__init__.py
+touch src/helpers.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "directory and files created successfully!!"
